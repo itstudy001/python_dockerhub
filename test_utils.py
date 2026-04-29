634c77d747579a19ceda61bbbc2fc60b1241f483
adam <itstudy@kakao.com>
2026-04-29 16:32:03 +0900

    kube_1

--- test_utils.py
+++ test_utils.py
@@ -3,5 +3,5 @@ from utils import get_movie_info
 def test_get_movie_info():
     test_url = "https://movie.naver.com/movie/bi/mi/basic.nhn?code=185293"
     title = get_movie_info(test_url)
-    assert title == "내일의 기억 : 네이버 검색"
+    assert title == "네이버 검색"
  
